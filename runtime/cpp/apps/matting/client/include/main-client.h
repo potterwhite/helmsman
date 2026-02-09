e7af853b47fd3ecae002cf233e80c1e55a56dba0
PotterWhite <themanuknowwhom@outlook.com>
2026-02-09 11:23:07 +0800

    v1.3.3 generated debug_v1.3.3_inference_execution.bin successfully it need to be confirm
    	new file:   runtime/cpp/apps/matting/client/include/main-client.h
    	modified:   runtime/cpp/apps/matting/client/src/CMakeLists.txt
    	modified:   runtime/cpp/apps/matting/client/src/main-client.cpp

---
+++ runtime/cpp/apps/matting/client/include/main-client.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// namespace arcforge {
+// namespace runtime {
+
+typedef struct {
+	std::vector<float> data;
+	int64_t height;
+	int64_t width;
+} TensorData;
+
+// }  // namespace runtime
+// }  // namespace arcforge
--- runtime/cpp/apps/matting/client/src/main-client.cpp
+++ runtime/cpp/apps/matting/client/src/main-client.cpp
@@ -19,8 +19,8 @@
 // SOFTWARE.
 
 #include "CVKit/base/base.h"
-#include "Utils/file/file-utils.h"
 #include "Runtime/onnx/onnx.h"
+#include "Utils/file/file-utils.h"
 #include "Utils/logger/logger.h"
 #include "Utils/logger/worker/consolesink.h"
 #include "Utils/logger/worker/filesink.h"
@@ -37,6 +37,7 @@
 #include <thread>
 #include <utility>
 #include <vector>
+#include "main-client.h"
 
 using namespace arcforge::embedded;
 
@@ -70,13 +71,50 @@ bool isRelease() {
 	return build_type == "Release";
 }
 
-// Ort::SessionOptions init_session_option(void) {
-// 	Ort::SessionOptions opt;
-// 	opt.SetIntraOpNumThreads(1);
-// 	opt.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_BASIC);
+TensorData& processing_pipeline(const std::string& imagePath, const std::string& outputBinPath,
+                                TensorData& tensor_data) {
+	auto cvkit_obj = std::make_unique<arcforge::cvkit::Base>();
 
-// 	return opt;
-// }
+	cv::Mat img = cvkit_obj->loadImage(imagePath);
+	img = cvkit_obj->bgrToRgb(img);
+	img = cvkit_obj->ensure3Channel(img);
+	/*
+	     * NOTE:
+	     * DO NOT use cv::normalize / convertTo here.
+	     * This must be bitwise identical to NumPy preprocessing.
+	     *
+	     * Date: Feb03.2026
+	     * Author: PotterWhite
+	     *
+	     * img = cvkit_obj->normalizeToMinusOneToOne(img);
+	     */
+	img = cvkit_obj->normalize_exact_numpy(img);
+	cvkit_obj->dumpBinary(img, outputBinPath + "/cpp_00_04_normalized.bin");
+
+	// 1. resize to fit model input size
+	constexpr int ref_size = 512;
+	auto scale_factor = math_utils.getScaleFactor(img.rows, img.cols, ref_size);
+	std::cout << std::setprecision(17) << "x_scale_factor=" << scale_factor.first
+	          << ", y_scale_factor=" << scale_factor.second << std::endl;
+	cv::resize(img,                  // src
+	           img,                  // dst（可以原地）
+	           cv::Size(),           // dsize 为空
+	           scale_factor.first,   // fx
+	           scale_factor.second,  // fy
+	           cv::INTER_AREA        // interpolation
+	);
+	logger.Info("Resized Width=" + std::to_string(img.cols) +
+	                ", Resized Height=" + std::to_string(img.rows),
+	            kcurrent_app_name);
+
+	// 2. convert to NCHW
+	// std::vector<float> result = hwcToNchw(img);
+	tensor_data.data = cvkit_obj->hwcToNchw(img, 3);
+	tensor_data.height = static_cast<int64_t>(img.rows);
+	tensor_data.width = static_cast<int64_t>(img.cols);
+
+	return tensor_data;
+}
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
 	// auto& logger = arcforge::embedded::utils::Logger::GetInstance();
@@ -103,9 +141,113 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
 
 	// setup signal handler
 	signal(SIGINT, SignalHandler);
-	// signal(SIGTERM, SignalHan/development/docker_volumes/src/ai/image-matting/helmsman.git/runtime/cpp/build/native-release/debug/dler);
+	// signal(SIGTERM, SignalHandler);
 
-	#if 0
+#if 1
+	// v1.3.3 inference execution
+	if (argc != 4) {
+		std::cerr << "Usage: infer <image_path> <onnx_path> <output_bin>\n";
+		return 1;
+	}
+
+	const std::string image_path = argv[1];
+	const std::string onnx_path = argv[2];
+	// const std::string input_bin_path = argv[3];
+	const std::string output_bin_path = argv[3];
+
+	try {
+		// ----------------
+		// Processing -- 1. create ONNX Runtime environment and session
+		Ort::Env env(ORT_LOGGING_LEVEL_WARNING, "v1.3.3-inference-execution");
+
+		Ort::Session session(env, onnx_path.c_str(), runtime.init_session_option());
+
+		Ort::AllocatorWithDefaultOptions allocator;
+
+		const char* input_name = session.GetInputName(0, allocator);
+		const char* output_name = session.GetOutputName(0, allocator);
+
+		std::vector<int64_t> input_shape =
+		    session.GetInputTypeInfo(0).GetTensorTypeAndShapeInfo().GetShape();
+
+		logger.Info("Input Name: " + std::string(input_name));
+		logger.Info("Output Name: " + std::string(output_name));
+
+		logger.Info("Input Shape: ");
+		for (auto s : input_shape) {
+			logger.Info(std::to_string(s) + " ");
+		}
+		logger.Info("\n");
+
+		// ----------------
+		// Processing -- 2. obtain std::vector<float> input tensor from preprocessing pipeline
+		TensorData tensor_data;
+		std::vector<float> input_tensor_values;
+		tensor_data = processing_pipeline(image_path, output_bin_path, tensor_data);
+		input_tensor_values = tensor_data.data;
+		logger.Info("Loaded input tensor size: " + std::to_string(input_tensor_values.size()));
+
+		//---------------
+		// Processing -- 3. process input tensor shape
+		int64_t N = 1;
+		int64_t C = 3;
+		int64_t H = tensor_data.height;
+		int64_t W = tensor_data.width;
+
+		std::vector<int64_t> real_input_shape = {N, C, H, W};
+
+		size_t input_tensor_size = static_cast<size_t>(N * C * H * W);
+
+		logger.Info("Input tensor size: " + std::to_string(input_tensor_size));
+		logger.Info("Input tensor actual size: " + std::to_string(input_tensor_values.size()));
+		if (input_tensor_size != input_tensor_values.size()) {
+			logger.Error("❌ Size mismatch! expected " + std::to_string(input_tensor_size) +
+			             " got " + std::to_string(input_tensor_values.size()));
+			return 1;
+		}
+
+		//----------------
+		//
+		Ort::MemoryInfo memory_info =
+		    Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+
+		Ort::Value input_tensor = Ort::Value::CreateTensor<float>(
+		    memory_info, input_tensor_values.data(), input_tensor_size, real_input_shape.data(),
+		    real_input_shape.size());
+
+		auto output_tensors =
+		    session.Run(Ort::RunOptions{nullptr}, &input_name, &input_tensor, 1, &output_name, 1);
+
+		float* output_data = output_tensors[0].GetTensorMutableData<float>();
+
+		auto output_shape = output_tensors[0].GetTensorTypeAndShapeInfo().GetShape();
+
+		logger.Info("Output Shape: ");
+		for (auto s : output_shape) {
+			logger.Info(std::to_string(s) + " ");
+		}
+		logger.Info("\n");
+
+		size_t output_tensor_size = 1;
+		for (auto s : output_shape) {
+			output_tensor_size *= static_cast<size_t>(s);
+		}
+
+		std::vector<float> output_vector(output_data, output_data + output_tensor_size);
+
+		file_utils.dumpBinary(output_vector,
+		                      output_bin_path + "debug_v1.3.3_inference_execution.bin");
+
+		logger.Info("✅ Inference done. Output dumped.");
+
+	} catch (const Ort::Exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+#endif
+
+#if 0
+	// v1.3.2 hwcToNchw
 	if (argc != 3) {
 		std::cerr << "Usage: preprocess_normalize <input_image> <output_bin>\n";
 		return 1;
@@ -158,9 +300,10 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		return 1;
 	}
-	#endif
+#endif
 
-	#if 1
+#if 0
+	// v1.3.1 Load and infer ONNX model
 	if (argc != 2) {
 		std::cerr << "Usage: load_onnx <onnx_path> \n";
 		return 1;
@@ -181,7 +324,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		return 1;
 	}
-	#endif
+#endif
 
 	std::cout << "hello " << kcurrent_app_name << std::endl;
 
