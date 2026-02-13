bd8b74bf250605711ac0a834aa614807e3a612e3
PotterWhite <themanuknowwhom@outlook.com>
2026-02-13 17:10:57 +0800

    wrote some comments in rknn.cpp and change rknn_init() with model path mode instead of binary mode
    	modified:   runtime/cpp/apps/matting/client/src/pipeline/inference-engine/rknn/rknn.cpp

--- runtime/cpp/apps/matting/client/src/pipeline/inference-engine/rknn/rknn.cpp
+++ runtime/cpp/apps/matting/client/src/pipeline/inference-engine/rknn/rknn.cpp
@@ -19,34 +19,55 @@ InferenceEngineRKNN::~InferenceEngineRKNN() {
 void InferenceEngineRKNN::load(const std::string& model_path) {
 	auto& logger = arcforge::embedded::utils::Logger::GetInstance();
 
-	// 1️⃣ 读取 rknn 文件
-	std::ifstream file(model_path, std::ios::binary | std::ios::ate);
-	// std::streamsize model_size = file.tellg();
-	size_t model_size = static_cast<size_t>(file.tellg());
-	file.seekg(0);
+	// // I. 读取 rknn 文件
+	// std::ifstream file(model_path, std::ios::binary | std::ios::ate);
+	// // std::streamsize model_size = file.tellg();
+	// size_t model_size = static_cast<size_t>(file.tellg());
+	// file.seekg(0);
 
-	std::vector<uint8_t> model_data(model_size);
-	file.read(reinterpret_cast<char*>(model_data.data()), static_cast<std::streamsize>(model_size));
-	file.close();
+	// std::vector<uint8_t> model_data(model_size);
+	// file.read(reinterpret_cast<char*>(model_data.data()), static_cast<std::streamsize>(model_size));
+	// file.close();
 
-	// 2️⃣ 初始化 rknn context
-	int ret = rknn_init(&ctx_, model_data.data(), static_cast<uint32_t>(model_size), 0, nullptr);
+	/* II. rknn context initialization
+	 * rknn_context *context:
+	 *		pointer of rknn_context。
+	 * void *model:
+	 * 		RKNN模型的二进制数据或者RKNN模型路径。
+	 * 		当参数size大于0时,model表示二进制数据;
+	 * 		当参数size等于0时,model表示RKNN模型路径。
+	 * uint32_t size:
+	 * 		当model是二进制数据,表示模型大小,当model是路径,则设置为0。
+	 * uint32_t flag:
+	 * 		初始化标志,默认初始化行为需要设置为0。
+	 * rknn_init_extend:
+	 * 		特定初始化时的扩展信息。没有使用,传入NULL即可。如果需要共享模型weight内存,则需要传入另个模型rknn_context指针。
+	*/
+	// ---------------
+	// 1. binary mode
+	// int ret = rknn_init(&ctx_, model_data.data(), static_cast<uint32_t>(model_size), 0, nullptr);
+	// ---------------
+	// 2. file path mode
+	int ret = rknn_init(&ctx_, const_cast<void*>(static_cast<const void*>(model_path.c_str())), 0,
+	                    0, nullptr);
 	if (ret < 0) {
 		throw std::runtime_error("rknn_init failed!");
+	} else {
+		logger.Info("RKNN model loaded and context initialized successfully.",
+		            kcurrent_module_name);
 	}
 
-	// 3️⃣ 查询 IO 数量
+	// III. 查询 IO 数量
 	rknn_query(ctx_, RKNN_QUERY_IN_OUT_NUM, &io_num_, sizeof(io_num_));
 
 	logger.Info("Input num: " + std::to_string(io_num_.n_input), kcurrent_module_name);
 	logger.Info("Output num: " + std::to_string(io_num_.n_output), kcurrent_module_name);
 
-	// 4️⃣ 查询 input attr
+	// IV. 查询 input & output attr
 	memset(&input_attr_, 0, sizeof(input_attr_));
 	input_attr_.index = 0;
 	rknn_query(ctx_, RKNN_QUERY_INPUT_ATTR, &input_attr_, sizeof(input_attr_));
 
-	// 5️⃣ 查询 output attr
 	memset(&output_attr_, 0, sizeof(output_attr_));
 	output_attr_.index = 0;
 	rknn_query(ctx_, RKNN_QUERY_OUTPUT_ATTR, &output_attr_, sizeof(output_attr_));
@@ -57,13 +78,12 @@ void InferenceEngineRKNN::load(const std::string& model_path) {
 	logger.Info("Input size bytes: " + std::to_string(input_size_), kcurrent_module_name);
 	logger.Info("Output size bytes: " + std::to_string(output_size_), kcurrent_module_name);
 
-	logger.Info(arcforge::runtime::to_string(input_attr_));
-	logger.Info(arcforge::runtime::to_string(output_attr_));
+	logger.Info("Input attr: " + arcforge::runtime::to_string(input_attr_));
+	logger.Info("Output attr: " + arcforge::runtime::to_string(output_attr_));
 
 	// ================================
-	// 🔥🔥🔥 ZERO-COPY BUFFER 创建
+	// V. ZERO-COPY BUFFER 创建
 	// ================================
-
 	input_mem_ = rknn_create_mem(ctx_, static_cast<uint32_t>(input_size_));
 	output_mem_ = rknn_create_mem(ctx_, static_cast<uint32_t>(output_size_));
 
