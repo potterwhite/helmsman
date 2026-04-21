4e2db515e361aaeca20a04b7a30940bbd382dc72
PotterWhite <themanuknowwhom@outlook.com>
2026-04-21 14:37:33 +0800

    refactor: reorganize pipeline directory structure to stages/

---
+++ runtime/cpp/apps/matting/server/include/pipeline/stages/backend/backend.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2026 PotterWhite
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <memory>
+#include <opencv2/opencv.hpp>
+#include "Utils/file/file-utils.h"
+#include "Utils/logger/logger.h"
+#include "Utils/logger/worker/consolesink.h"
+#include "Utils/logger/worker/filesink.h"
+#include "Utils/math/math-utils.h"
+#include "pipeline/stages/backend/post-processor/i-post-processor.h"
+#include "common/data_structure.h"
+
+class MattingBackend {
+   public:
+	MattingBackend();
+	~MattingBackend();
+
+	void setOutputPath(const std::string& path);
+	void setBackgroundPath(const std::string& path);
+	void setForegroundImagePath(const std::string& path);
+
+	/**
+	 * Attach an optional post-processor (e.g. GuidedFilterPostProcessor).
+	 * Pass nullptr to disable post-processing and use the raw alpha mask.
+	 */
+	void setPostProcessor(std::shared_ptr<IPostProcessor> processor);
+
+	// Accept multi-tensor output from InferenceEngine.
+	// Selects the pha (alpha matte) tensor by name or position:
+	//   - MODNet: outputs[0] = pha
+	//   - RVM:    outputs[1] = pha (outputs[0] = fgr)
+	cv::Mat postprocess(const std::vector<TensorData>& outputs);
+
+   private:
+	std::string output_path_;
+	std::string background_path_;
+	std::string foreground_image_path_;
+
+	std::shared_ptr<IPostProcessor> post_processor_;  // nullptr = no post-processing
+
+	cv::Mat nchwToHwc(const TensorData& tensor);
+};
---
+++ runtime/cpp/apps/matting/server/include/pipeline/stages/frontend.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2026 PotterWhite
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include "CVKit/base/base.h"
+#include "Utils/file/file-utils.h"
+#include "Utils/logger/logger.h"
+#include "Utils/logger/worker/consolesink.h"
+#include "Utils/logger/worker/filesink.h"
+#include "Utils/math/math-utils.h"
+#include "common/data_structure.h"
+
+class ImageFrontend {
+   public:
+	ImageFrontend();
+	~ImageFrontend();
+	// static ImageFrontend& GetInstance();
+	TensorData preprocess(const std::string& image_path, size_t model_width = 1024,
+	                      size_t model_height = 576);
+	TensorData preprocess(const cv::Mat& bgr_frame, size_t model_width = 1024,
+	                      size_t model_height = 576);
+
+	// getter and setter
+	void setOutputBinPath(const std::string& path);
+
+   private:
+	// member functions
+	TensorData _preprocessCore(cv::Mat img, size_t model_width, size_t model_height);
+
+   private:
+	// member variables
+	std::string outputBinPath_ = "";
+	arcforge::cvkit::Base cvkit_;  // Reused across frames (stateless utility)
+};
